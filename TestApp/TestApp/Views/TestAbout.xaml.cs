6076437b016a616f76505460602caef5498db37f
jackfrost0 <jackfrost0@ksu.edu>
2021-05-31 15:28:16 -0500

    Add project files.

---
+++ TestApp/TestApp/Views/TestAbout.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class TestAbout : ContentPage
+    {
+        public TestAbout()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            // Launch the specified URL in the system browser.
+            await Launcher.OpenAsync("https://aka.ms/xamarin-quickstart");
+        }
+    }
+}
